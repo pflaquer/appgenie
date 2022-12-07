ab1ab2cdd225b564ebc9453627a81cb0829a88ff
Dromance <88290916+pflaquer@users.noreply.github.com>
2022-12-07 11:45:01 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,13 @@
   <section class="home">
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
+      <div class="flex flex-col w-full xl:w-2/5">
+        <img
+          alt="Hero"
+          class="rounded shadow-xl"
+          src="https://getvectorlogo.com/wp-content/uploads/2018/11/the-genie-company-vector-logo.png"
+        />
+      </div>
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
         <div class="mb-12 xl:mb-0">
